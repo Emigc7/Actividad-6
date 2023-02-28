e16a6e20964d3bff6bc35afce09353411687192c
Emilio <emigc7@gmail.com>
2023-02-28 23:30:41 +0100

    Inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FormularioComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
